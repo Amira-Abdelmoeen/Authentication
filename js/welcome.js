ad1fe5636714c243870f22729dc35a58d58e7775
Amira-Abdelmoeen <amiraamur300@gmail.com>
2024-01-07 22:03:02 +0200

    Edit buttons

--- js/welcome.js
+++ js/welcome.js
@@ -1,15 +1,12 @@
 
 var user = JSON.parse(localStorage.getItem("User"))
 
-
-
-
 document.getElementById("welcome").innerHTML+= " " + user.name
 
 
 function logOut(){
     localStorage.removeItem("User")
-    // window.location.href="../Login.html"
+    
     window.location.href="/index.html"
 }
 
