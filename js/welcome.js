c2f06b473b01aef18327fc6395228b92f5ceb82f
Amira-Abdelmoeen <amiraamur300@gmail.com>
2024-01-07 18:41:33 +0200

    edit log out2

--- js/welcome.js
+++ js/welcome.js
@@ -10,6 +10,6 @@ document.getElementById("welcome").innerHTML+= " " + user.name
 function logOut(){
     localStorage.removeItem("User")
     // window.location.href="../Login.html"
-    window.location.href="index.html"
+    window.location.href="/index.html"
 }
 
